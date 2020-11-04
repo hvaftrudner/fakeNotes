032e9848f578f295739b6ee427b00ed47cd9c5b2
hvaftrudner <krill_eriksson@hotmail.com>
2020-11-04 19:45:48 +0100

    Initial Commit

---
+++ fakeNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fakeNotes
+//
+//  Created by Kristoffer Eriksson on 2020-11-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
